6165613468f7b8f9ac058dd309919cc68426ed85
esecharly <62493210+esecharly@users.noreply.github.com>
2021-04-15 05:41:04 -0500

    create insertarDatos.php

--- crud/crud.php
+++ crud/crud.php
@@ -15,5 +15,19 @@
             return $query->fetchAll();
             $query = null;   
         }
+
+        public function insertarDatos($datos){
+            $sql = "INSERT INTO t_crud (nombre, sueldo, edad, fRegistro)
+                                VALUES (:nombre, :sueldo, :edad, :fRegistro)";
+            $query = Conexion::conectar()->prepare($sql);
+            $query->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);                    
+            $query->bindParam(":sueldo", $datos["sueldo"], PDO::PARAM_STR);                    
+            $query->bindParam(":edad", $datos["edad"], PDO::PARAM_INT);                    
+            $query->bindParam(":fRegistro", $datos["fecha"], PDO::PARAM_STR);  
+            
+            return $query->execute();
+
+            $query = null;
+        }
     }
 ?>
--- procesos/insertarDatos.php
+++ procesos/insertarDatos.php
@@ -2,6 +2,9 @@
     require_once "../crud/crud.php";
 
     $datos = array(
-
+        'nombre'=> $_POST['nombre'],
+        'sueldo'=> $_POST['sueldo'],
+        'edad'=> $_POST['edad'],
+        'fecha'=> $_POST['fecha']
     );
 ?>
